5482a4cbd8d2e8e161487b0d5f55f70e0d5514c5
waynegoh97 <waynegoh97@gmail.com>
2023-12-11 18:30:04 +0800

    Added main feature for rock paper scissors game

---
+++ index.js
@@ -0,0 +1,58 @@
+const choice = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+  return choice.at(Math.floor(Math.random()*3));
+}
+
+function playerSelection(){
+  let select = prompt("Rock Paper or Scissors?").toLowerCase();
+  return select
+}
+
+function playRound(player, computer){
+  switch(player) {
+    case "rock":
+      if (computer == "scissors"){
+        return "You Win! Rock beats Scissors";
+      }
+      else if (computer == "paper"){
+        return "You Lose! Rock lose to Paper";
+      }
+      else{
+        return "Draw!";
+      }
+      
+    case "paper":
+      if (computer == "rock"){
+        return "You Win! Paper beats Rock";
+      }
+      else if (computer == "scissors"){
+        return "You Lose! Paper lose to Scissors";
+      }
+      else {
+        return "Draw!";
+      }
+
+    case "scissors":
+      if (computer == "paper"){
+        return "You Win! Scissors beats Paper";
+      }
+      else if (computer == "rock"){
+        return "You Lose! Scissors lose to Rock";
+      }
+      else{
+        return "Draw!";
+      }
+
+    default:
+      return "Incorrect Input!"
+
+  }
+}
+
+
+
+let player = playerSelection();
+let comp = getComputerChoice();
+
+console.log(playRound(player, comp))
